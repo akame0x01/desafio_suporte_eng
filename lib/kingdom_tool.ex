c2ef2664ab5f14ee245d42bf5bfd406234000100
アカメ <mooraesz123@gmail.com>
2024-07-17 18:09:50 +0000

    Almost finishing, the logic to print is incorrect tho
    * Progressed a lot today, but i figure out that i was doing the wrong
      think, i would correct and refactor the code tomorrow. Also there are
      some copied and pasted code to convert the number to roman that i
      don't understand a bit yet.
    * tasks.txt is not being used anymore since i was too focused in this
      new language stuff that i forgot to break down the tasks audhaduha

--- lib/kingdom_tool.ex
+++ lib/kingdom_tool.ex
@@ -1,62 +1,61 @@
-#
-# ferramenta de linha de comando que você irá desenvolver deve receber a lista dos reis e rainhas de Cumbúquia em ordem e retornar os mesmos nomes, cada um com sua devida numeração.
-#
-# Ao iniciar o binário, ele deve primeiro exibir uma breve explicação de seu uso. Ele irá então esperar o usuário inserir uma lista de nomes. Os nomes serão inseridos um por linha, e a lista será considerada terminada quando for lida uma linha em branco.
-#
-# Após a lista ser finalizada, a ferramenta irá repetir os nomes inseridos, porém, os nomes serão acrescidos de um número romano conforme necessário.
-#
-
 defmodule KingdomTool do
-  @moduledoc """
-  Documentation for `KingdomTool`.
-  """
+  @mappings [{"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}]
 
-  @doc """
-  Hello world.
+  def convert(number) when number > 1 do
+    {0, str} =
+      Enum.reduce(@mappings, {number, ""}, fn {roman, arabic}, {number, str} ->
+        {rem(number, arabic), str <> String.duplicate(roman, div(number, arabic))}
+      end)
 
-  ## Examples
+    str
+  end
 
-      iex> KingdomTool.hello()
-      :world
+  def convert(number) do 
+      ""
+  end
 
-  """
   def start do
-    KingdomTool.how_to_use()
     names_list = []
-    names_list = KingdomTool.get_inputs(names_list)
-    IO.inspect(names_list, label: "Names_list", charlists: :as_lists)
-    names_list = KingdomTool.check_for_same_name(names_list)
-    IO.inspect(names_list, label: "Names_list", charlists: :as_lists)
+    KingdomTool.how_to_use()
+    KingdomTool.get_inputs(names_list) 
+    |> KingdomTool.format_to_roman() 
+    |> KingdomTool.print_list_to_user()
   end
 
   def how_to_use do
-    IO.puts("explaining...\n")
+    IO.puts("explaining......\n")
   end
 
   def get_inputs(list) do
-    # Get User inputs, remove any blank characters and if it's not empty save it to a list.
-    input = IO.gets("> ") |> String.trim()
-    
-    # Names does have numbers, remove any numbers in the input and if there are only numbers the result will be an empty string.
-    input = String.replace(input, ~r/\d/,"")
-
-    if input == "" do
-      list = Enum.reverse(list)
+    name_input = IO.gets("> ") 
+      |> String.trim() 
+      |> String.replace(~r/\d/,"")
+  
+    if name_input == "" do
+      Enum.reverse(list)
     else
-      list = [input | list]
+      list = [name_input | list]
       KingdomTool.get_inputs(list)
     end
   end
 
-  def check_for_same_name(names_list) do
-    # If we have the same name in the list provided by the user, put an romane number in the final. 
-    #String.downcase(string) |> String.normalize(:nfkd)
-    KingdomTool.print_list_to_user()
+  def format_to_roman(names_list) do
+    names_map = Enum.frequencies(names_list)
+    names_uniq = Enum.uniq(names_list)
+
+    formated_list = []
+    formated_list = Enum.reduce(names_uniq, formated_list, fn element,acc ->
+      n_name = Map.get(names_map,element)
+      formated_name = element <> " " <> KingdomTool.convert(n_name)
+      [formated_name | acc]
+    end) |> Enum.reverse()
+
+    IO.inspect(formated_list, label: "formated_list", charlists: :as_lists)
+    formated_list
   end
 
-  def print_list_to_user do
-    # Print the formated list 
+  def print_list_to_user(names_list_formated) do
+    Enum.join(names_list_formated,"\n") |> IO.puts()
   end
 end
-
 KingdomTool.start()
