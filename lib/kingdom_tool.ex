704bb6c7b743275edccddc0d4591225e7d326c52
アカメ <mooraesz123@gmail.com>
2024-07-19 15:38:02 +0000

    GET IT ALMOST FINISH ITS WORKING AUDHAUDHAIWDNOAIWDNIAW

--- lib/kingdom_tool.ex
+++ lib/kingdom_tool.ex
@@ -1,17 +1,13 @@
 defmodule KingdomTool do
   @mappings [{"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}]
 
-  def convert(number) when number > 1 do
+  def convert(number) when number >= 1 do
     {0, str} =
       Enum.reduce(@mappings, {number, ""}, fn {roman, arabic}, {number, str} ->
         {rem(number, arabic), str <> String.duplicate(roman, div(number, arabic))}
       end)
 
     str
-  end
-
-  def convert(number) do 
-      ""
   end
 
   def start do
@@ -27,9 +23,11 @@ defmodule KingdomTool do
   end
 
   def get_inputs(list) do
-    name_input = IO.gets("> ") 
+    name_input = IO.gets("> ")
+      |> String.capitalize()
       |> String.trim() 
       |> String.replace(~r/\d/,"")
+
   
     if name_input == "" do
       Enum.reverse(list)
@@ -41,21 +39,34 @@ defmodule KingdomTool do
 
   def format_to_roman(names_list) do
     names_map = Enum.frequencies(names_list)
-    names_uniq = Enum.uniq(names_list)
+    {count, result} = {names_map, []}
+    starting_0 = Enum.into(Enum.map(names_map, fn ({key, _value}) -> {key, 1} end), %{})
 
-    formated_list = []
-    formated_list = Enum.reduce(names_uniq, formated_list, fn element,acc ->
-      n_name = Map.get(names_map,element)
-      formated_name = element <> " " <> KingdomTool.convert(n_name)
-      [formated_name | acc]
-    end) |> Enum.reverse()
+    {_count, result, starting_0} = Enum.reduce(names_list, {count,result, starting_0}, fn (element,{record_times,acc, starting_0}) ->
+      number_of_reps = Map.get(names_map,element)
+      # name is repeated.
+      if (number_of_reps > 1) do
+          number_of_reps = Map.get(starting_0,element)
+          formated_name = element <> " " <> KingdomTool.convert(number_of_reps)
 
-    IO.inspect(formated_list, label: "formated_list", charlists: :as_lists)
+          starting_0 = Map.replace(starting_0,element,number_of_reps + 1)
+          {record_times, [formated_name | acc], starting_0}
+      else
+          {record_times, [ element  | acc], starting_0} 
+      end
+
+    end)
+
+    formated_list = Enum.reverse(result)
+
     formated_list
   end
 
   def print_list_to_user(names_list_formated) do
     Enum.join(names_list_formated,"\n") |> IO.puts()
   end
+
 end
+
+
 KingdomTool.start()
