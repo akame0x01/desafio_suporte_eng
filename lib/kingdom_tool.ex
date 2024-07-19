d26ee331a5736a73e76e583a170c384342f09d4d
アカメ <mooraesz123@gmail.com>
2024-07-19 16:12:14 +0000

    Challenge finished.
    * Deleted README.md cause they for sure know how to run the program and
      there are no deps in this challenge so there is no need for readme

--- lib/kingdom_tool.ex
+++ lib/kingdom_tool.ex
@@ -1,25 +1,16 @@
 defmodule KingdomTool do
   @mappings [{"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}]
-
-  def convert(number) when number >= 1 do
-    {0, str} =
-      Enum.reduce(@mappings, {number, ""}, fn {roman, arabic}, {number, str} ->
-        {rem(number, arabic), str <> String.duplicate(roman, div(number, arabic))}
-      end)
-
-    str
-  end
-
+ 
   def start do
     names_list = []
     KingdomTool.how_to_use()
     KingdomTool.get_inputs(names_list) 
-    |> KingdomTool.format_to_roman() 
+    |> KingdomTool.format_names_to_roman() 
     |> KingdomTool.print_list_to_user()
   end
 
   def how_to_use do
-    IO.puts("explaining......\n")
+    IO.puts("Esta ferramenta organiza e numera uma lista de reis e rainhas de Cumbúquia. Ela recebe os nomes em ordem cronológica e os retorna com a devida numeração em algarismos romanos.\n\nPor favor, insira os nomes abaixo, caso nao queira inserir mais nomes, deixe a linha em branco e aperte Enter.\n")
   end
 
   def get_inputs(list) do
@@ -28,7 +19,6 @@ defmodule KingdomTool do
       |> String.trim() 
       |> String.replace(~r/\d/,"")
 
-  
     if name_input == "" do
       Enum.reverse(list)
     else
@@ -37,36 +27,34 @@ defmodule KingdomTool do
     end
   end
 
-  def format_to_roman(names_list) do
+  def from_int_to_roman(number) when number >= 1 do
+    {0, str} =
+      Enum.reduce(@mappings, {number, ""}, fn {roman, arabic}, {number, str} ->
+        {rem(number, arabic), str <> String.duplicate(roman, div(number, arabic))}
+      end)
+    str
+  end
+
+  def format_names_to_roman(names_list) do
     names_map = Enum.frequencies(names_list)
-    {count, result} = {names_map, []}
-    starting_0 = Enum.into(Enum.map(names_map, fn ({key, _value}) -> {key, 1} end), %{})
+    names_map_values_to_one = Enum.into(Enum.map(names_map, fn ({key, _value}) -> {key, 1} end), %{})
+    result = []
 
-    {_count, result, starting_0} = Enum.reduce(names_list, {count,result, starting_0}, fn (element,{record_times,acc, starting_0}) ->
-      number_of_reps = Map.get(names_map,element)
-      # name is repeated.
-      if (number_of_reps > 1) do
-          number_of_reps = Map.get(starting_0,element)
-          formated_name = element <> " " <> KingdomTool.convert(number_of_reps)
+    {result, _names_map_values_to_one} = Enum.reduce(names_list, {result, names_map_values_to_one}, fn (element,{accumulator_result, map_to_inc_rep}) ->
+          number_of_reps = Map.get(map_to_inc_rep,element)
+          formated_name = element <> " " <> KingdomTool.from_int_to_roman(number_of_reps)
 
-          starting_0 = Map.replace(starting_0,element,number_of_reps + 1)
-          {record_times, [formated_name | acc], starting_0}
-      else
-          {record_times, [ element  | acc], starting_0} 
-      end
+          map_to_inc_rep = Map.replace(map_to_inc_rep,element,number_of_reps + 1)
+          {[formated_name | accumulator_result], map_to_inc_rep}
 
     end)
-
     formated_list = Enum.reverse(result)
-
     formated_list
   end
 
   def print_list_to_user(names_list_formated) do
     Enum.join(names_list_formated,"\n") |> IO.puts()
   end
-
 end
-
 
 KingdomTool.start()
